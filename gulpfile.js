6cb35697c2bbd04b16a9b4efadd9e7f52a235369
Marko <marko.hamppula@metropolia.fi>
2014-12-09 15:19:05 +0200

    gulp added

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+var gulp = require("gulp");
+var jshint = require("gulp-jshint");
+
+
+gulp.task("default", ['lint']);
+
+gulp.task("lint", function() {
+    gulp.src("./*.js")
+        .pipe(jshint())
+        .pipe(jshint.reporter("default"));
+});
